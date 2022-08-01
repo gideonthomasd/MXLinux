afcb95710dbab9362a9e8eaf777b7c83b915b815
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-08-01 20:06:53 +0100

    Add files via upload

---
+++ 100_Install_software.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#sudo apt install -y i3 bspwm i3lock-fancy plank lxde appimagelauncher conky
+
+sudo apt install -y fonts-font-awesome fonts-fork-awesome xfce4-terminal thunar gdebi polybar feh pulsemixer neofetch sxhkd curl dmenu rofi pcmanfm picom lxtask lxappearance lxpolkit aptitude moc lxterminal geany
+sudo apt install -y make gcc dunst libx11-dev libxft-dev libxinerama-dev fonts-noto-mono meson cmake libpulse-dev libcxxopts-dev fonts-jetbrains-mono htop libnotify-bin
+sudo apt install -y deepin-icon-theme parcellite calcurse feh xterm lolcat moka-icon-theme
+
+curl -sS https://starship.rs/install.sh | sh
+
+

ed0a0cb0c29ca6b6fbfe11f09359604fd8f3994c
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-08-02 12:08:02 +0100

    Add files via upload

---
+++ 120_Install_i3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install i3 i3lock-fancy
+
+cd i3
+cd polybar
+chmod +x *.sh
+cd ..
+cd bin
+chmod +x *.sh
+cd ..
+cd ..
+
+mkdir -p ~/.config/i3
+cd i3
+cp -r * ~/.config/i3
+cd ..

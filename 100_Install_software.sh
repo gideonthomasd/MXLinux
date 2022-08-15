0e9b93602f377298c1c76029789241ab81473e58
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-08-15 20:55:38 +0100

    Update 100_Install_software.sh

--- 100_Install_software.sh
+++ 100_Install_software.sh
@@ -4,7 +4,7 @@
 
 sudo apt install -y awesome fonts-font-awesome fonts-fork-awesome xfce4-terminal thunar gdebi polybar feh pulsemixer neofetch sxhkd curl dmenu rofi pcmanfm picom lxtask lxappearance lxpolkit aptitude moc lxterminal geany
 sudo apt install -y make gcc dunst libx11-dev libxft-dev libxinerama-dev fonts-noto-mono meson cmake libpulse-dev libcxxopts-dev fonts-jetbrains-mono htop libnotify-bin
-sudo apt install -y deepin-icon-theme parcellite calcurse feh xterm lolcat moka-icon-theme clementine font-manager
+sudo apt install -y deepin-icon-theme parcellite calcurse feh xterm lolcat moka-icon-theme clementine font-manager mpv
 
 curl -sS https://starship.rs/install.sh | sh
 
